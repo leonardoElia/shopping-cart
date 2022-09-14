f4dfff0d2fad08eb9337dbff9af3f5b9317df8c8
Leonardo Elias Souza de Oliverira Meireles <leonardoelias802@gmail.com>
2022-09-14 18:11:01 -0300

    Fazendo 6 requesitos obrigatorios

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -1,10 +1,19 @@
 const localStorageSimulator = require('../mocks/localStorageSimulator');
 const saveCartItems = require('../helpers/saveCartItems');
+//const { expect } = require('chai');
+// const { expect } = require('chai');
 
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  it('testando se passar um intem o localStorage.setItem é usado',  async () => {
-    await saveCartItems() 
+  it('testando se ao passar um item o localStorage.setItem é usado', () => {
+    saveCartItems();
+    expect(localStorage.setItem).toHaveBeenCalled() ;
+  });
+
+  it('testando de a função saveCartItem é executada de maneira correta', () => {
+    const item = 'carrinhoItem';
+    saveCartItems('carrinhoItem');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', JSON.stringify(item));
   });
 });
